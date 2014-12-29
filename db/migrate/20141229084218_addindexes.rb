b1fcd41dd11517a8a6041b2c4225b14c0aadd961
Alex Beznos <beznosa@yahoo.com>
2014-12-29 19:23:13 +0200

    Unique indexes

--- db/migrate/20141229084218_addindexes.rb
+++ db/migrate/20141229084218_addindexes.rb
@@ -1,5 +1,5 @@
 class Addindexes < ActiveRecord::Migration
-  add_index :notifications, :user_id, unique: true
-  add_index :messages, :user_id, unique: true
-  add_index :messages, :topic_id, unique: true
+  add_index :notifications, :user_id
+  add_index :messages, :user_id
+  add_index :messages, :topic_id
 end
